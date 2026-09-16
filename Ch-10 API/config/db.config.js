272b6ebe0f8ce472a28984c44cfcba40efd01588
Maulik Makvana <144558219+maulikmakvana@users.noreply.github.com>
2026-09-16 15:37:26 +0530

    Delete Ch-10 API directory

--- Ch-10 API/config/db.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("MongoDB is connected successfully...");
-}).catch((err) => {
-    console.log("MongoDB Connection Error: ", err);
-});
