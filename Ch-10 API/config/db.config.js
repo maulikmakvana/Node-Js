d7945387c91e8b5f8a2ca0fa60077995e812c2d3
Maulik Makvana <makvanamaulik0@gmail.com>
2026-09-16 15:23:28 +0530

    Add Node JS files

---
+++ Ch-10 API/config/db.config.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("MongoDB is connected successfully...");
+}).catch((err) => {
+    console.log("MongoDB Connection Error: ", err);
+});
